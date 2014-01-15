6af074b559b9d494b343ce0d2cf4737c7a6998c7
xeoleo <xeoleo@163.com>
2014-01-15 13:11:44 +0800

    Initial Commit

---
+++ Base64_AES_MD5/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Base64_AES_MD5
+//
+//  Created by ectrip on 14-1-15.
+//  Copyright (c) 2014年 ectrip. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
